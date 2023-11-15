cb2a324ee71e0884a33cc48f24888d9b195cd81d
Erdabi <yusuf.khurshid@gmx.ch>
2023-11-15 11:18:27 +0100

    Implemented login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from "@angular/material/card";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {RegisterDto, UserControllerService} from "../../../openapi-client";
+
 
 @Component({
   selector: 'pm-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  formGroup!: FormGroup;
 
+
+  constructor(private fb: FormBuilder,
+    private userService: UserControllerService) {
+this.formGroup = this.fb.group({
+email: ['', [Validators.required, Validators.email]],
+password: ['', [Validators.required, Validators.minLength(8)]]
+});
+}
+
+submit() {
+console.log(this.formGroup);
+console.log(this.formGroup.errors);
+if(this.formGroup.valid) {
+this.userService.login(this.formGroup.value as RegisterDto).subscribe(token=> {
+console.log(token)
+localStorage.setItem('ACCESS_TOKEN', token.token as string);
+alert('erfolgreich eingeloggt');
+})
+}
+}
 }
